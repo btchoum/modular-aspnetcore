a62d5c8a2e2b45bf70c47e6b11a185c67c04faaf
btchoum <bertrand.tchoumkeu@gmail.com>
2020-09-26 14:50:28 -0500

    add functional tests

--- tests/WebFrontEnd.Tests/SampleFunctionalTests.cs
+++ tests/WebFrontEnd.Tests/SampleFunctionalTests.cs
@@ -1,13 +1,35 @@
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Testing;
 using Xunit;
 
 namespace WebFrontEnd.Tests
 {
-    public class SampleFunctionalTests
+    public class SampleFunctionalTests : IClassFixture<WebApplicationFactory<Startup>>
     {
-        [Fact]
-        public void Test1()
-        {
+        private readonly WebApplicationFactory<Startup> _factory;
 
+        public SampleFunctionalTests(WebApplicationFactory<Startup> factory)
+        {
+            _factory = factory;
+        }
+
+        [Theory]
+        [InlineData("/")]
+        [InlineData("/Catalog")]
+        [InlineData("/Index")]
+        [InlineData("/Tickets")]
+        public async Task Get_returns_success_and_correct_content_type(string url)
+        {
+            // Arrange
+            var client = _factory.CreateClient();
+
+            // Act
+            var response = await client.GetAsync(url);
+
+            // Assert
+            response.EnsureSuccessStatusCode(); // Status Code 200-299
+            Assert.Equal("text/html; charset=utf-8",
+                response.Content.Headers.ContentType.ToString());
         }
     }
 }
